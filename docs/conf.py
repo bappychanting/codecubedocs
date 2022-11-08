eaa9aefb936dcdd0444b667425e2d18339905604
bappychanting <bappychanting@outlook.com>
2022-11-08 19:14:31 +0600

    updated configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 html_title = "CodeCube"
-html_logo = "https://user-images.githubusercontent.com/22908406/197393844-742ec334-eccb-4c52-bc0c-5192ad418a8c.png"
+html_logo = "https://user-images.githubusercontent.com/22908406/200573841-1b090676-6692-4762-9657-782dd98b56ab.png"
 html_favicon = "https://user-images.githubusercontent.com/22908406/200559448-e76b9b36-89aa-4463-b9a1-72c330dc1dba.png"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 html_sidebars = {
     '**': [
         'globaltoc.html',
